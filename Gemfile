a7d8d8ddf170365fe50c2cffcee85dbdf202e346
Francesca Santoriello <f.santoriello@hotmail.com>
2021-09-24 15:05:19 +0200

    config api

--- Gemfile
+++ Gemfile
@@ -47,3 +47,5 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 # Custom gems
 gem 'simple_form'
+gem 'dotenv-rails', groups: [:development, :test]
+gem 'marvel_api'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+
+  private
+  
+  @client = Marvel::Client.new
+  
+  @client.configure do |config|
+    config.api_key = ENV['MARVEL_PUBLIC_KEY']
+    config.private_key = ENV['MARVEL_PRIVATE_KEY']
+  end
+  
+  BASE_URL = 'https://gateway.marvel.com/'
 end
