88bdbd420285178e4ed7069cfa530be5f6bf7c28
Francesca Santoriello <f.santoriello@hotmail.com>
2021-09-25 12:12:43 +0200

    config api

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :config_client
 
-  private
-  
-  @client = Marvel::Client.new
-  
-  @client.configure do |config|
-    config.api_key = ENV['MARVEL_PUBLIC_KEY']
-    config.private_key = ENV['MARVEL_PRIVATE_KEY']
+  private 
+
+  def config_client    
+    @client = Marvel::Client.new
+    
+    @client.configure do |config|
+      config.api_key = ENV['MARVEL_PUBLIC_KEY']
+      config.private_key = ENV['MARVEL_PRIVATE_KEY']
+    end
   end
-  
-  BASE_URL = 'https://gateway.marvel.com/'
 end
