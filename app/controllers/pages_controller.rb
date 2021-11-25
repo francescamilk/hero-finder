c2b823ab47d4c76ee74c9d227296f93958529096
Francesca Santoriello <f.santoriello@hotmail.com>
2021-11-25 20:54:17 +0100

    ajax

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,14 @@
 class PagesController < ApplicationController
-  def home; end
+  def home
+    @characters = @client.characters(nameStartsWith: params[:query], orderBy: 'name')
+    @comics = @client.comics(titleStartsWith: params[:query], orderBy: 'title')
+    @series = @client.series(titleStartsWith: params[:query], orderBy: 'title')
+    @creators = @client.creators(lastName: params[:query], orderBy: 'lastName')
+    
+    
+    respond_to do |format|
+      format.html # Follow regular flow of Rails
+      format.text { render partial: 'list.html', locals: { movies: @movies } }
+    end
+  end
 end
--- app/controllers/result_controller.rb
+++ app/controllers/result_controller.rb
@@ -1,8 +1,3 @@
 class ResultController < ApplicationController
-  def index
-    @characters = @client.characters(nameStartsWith: params[:query], orderBy: 'name')
-    @comics = @client.comics(titleStartsWith: params[:query], orderBy: 'title')
-    @series = @client.series(titleStartsWith: params[:query], orderBy: 'title')
-    @creators = @client.creators(lastName: params[:query], orderBy: 'lastName')
-  end
+  def show; end
 end
