9050087bc3697eb4368604fc22370e647d4b7bb0
Francesca Santoriello <f.santoriello@hotmail.com>
2021-11-25 21:30:48 +0100

    working/noalign

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
   def home
+    
     @characters = @client.characters(nameStartsWith: params[:query], orderBy: 'name')
     @comics = @client.comics(titleStartsWith: params[:query], orderBy: 'title')
     @series = @client.series(titleStartsWith: params[:query], orderBy: 'title')
